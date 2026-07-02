42ee1f20f8ef63693a65941768b6b53bae383d5a
Nishanth TK <nishanthtk509@gmail.com>
2026-07-02 23:43:39 +0530

    second

--- streamvid_config/settings.py
+++ streamvid_config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5f2)8e8wbsy2dk5z!e#4v8jp7=284sqfe)1+c+&-xn=lnd_%yw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
