b0045800312d237b1bfcfd3b18dc1f97d98aa1a0
Nishanth TK <nishanthtk509@gmail.com>
2026-07-03 15:38:27 +0530

    admin loginsss555

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Admin(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
 
 class Signup(models.Model):
     username = models.CharField(max_length=100, unique=True)
